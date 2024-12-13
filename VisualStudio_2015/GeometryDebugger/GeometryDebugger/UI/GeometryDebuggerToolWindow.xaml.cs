65b9588f8001346240c5461d62ae9458a586b01d
MypkaXD <nicklayschannel@yandex.ru>
2024-12-13 21:04:58 +0300

    Added ToolTip and tried to fix subscribe on enterBreakMod

---
+++ VisualStudio_2015/GeometryDebugger/GeometryDebugger/UI/GeometryDebuggerToolWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Controls;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using System.Windows;
+
+namespace GeometryDebugger.UI
+{
+    public partial class GeometryDebuggerToolWindow : UserControl
+    {
+        private DebuggerEvents m_DE_debuggerEvents;
+
+        public GeometryDebuggerToolWindow()
+        {
+            InitializeComponent();
+
+            Loaded += GeometryDebuggerToolWindowLoaded;
+        }
+
+        public void GeometryDebuggerToolWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            SubscribeOnDebugEvents();
+        }
+
+
+        public void SubscribeOnDebugEvents()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var dte = Package.GetGlobalService(typeof(DTE)) as DTE;
+            if (dte == null)
+            {
+                Debug.WriteLine("DTE сервис не найден.");
+                return;
+            }
+
+            m_DE_debuggerEvents = dte.Events.DebuggerEvents;
+            m_DE_debuggerEvents.OnEnterBreakMode += OnEnterBreakMode;
+            Debug.WriteLine("Подписка на события DebuggerEvents выполнена.");
+        }
+
+        public void UnsubscribeFromDebugEvents()
+        {
+            if (m_DE_debuggerEvents != null)
+            {
+                m_DE_debuggerEvents.OnEnterBreakMode -= OnEnterBreakMode;
+                Debug.WriteLine("Отписка от событий DebuggerEvents выполнена.");
+            }
+        }
+
+        private void OnEnterBreakMode(dbgEventReason reason, ref dbgExecutionAction action)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            Debug.WriteLine($"EnterBreakMode: reason={reason}");
+        }
+    }
+}
---
+++ VisualStudio_2015/GeometryDebugger/GeometryDebugger/Utils/GeometryDebuggerControl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+using EnvDTE;
+using GeometryDebugger.UI;
+
+namespace GeometryDebugger.Utils
+{
+    [Guid("2af6a638-bbf3-4d1d-aab8-6ccf3491edbe")]
+    public class GeometryDebuggerControl : ToolWindowPane
+    {
+        private GeometryDebuggerToolWindow m_ui;
+
+        public GeometryDebuggerControl() : base(null)
+        {
+            this.Caption = "GeometryDebuggerTool";
+            m_ui = new GeometryDebuggerToolWindow();
+            this.Content = m_ui;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            // Убедиться, что выполняется в UI-потоке
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // Подписка на события
+            m_ui.SubscribeOnDebugEvents();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Отписываемся от событий, если требуется
+                m_ui?.UnsubscribeFromDebugEvents();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
