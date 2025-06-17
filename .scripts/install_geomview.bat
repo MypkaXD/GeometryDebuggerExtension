a341fcc5c8e37d7621628399edf5f9be7febe79f
MypkaXD <nicklayschannel@yandex.ru>
2025-06-17 21:29:58 +0300

    test

--- .scripts/install_geomview.bat
+++ .scripts/install_geomview.bat
@@ -27,7 +27,7 @@ if not exist "%INSTALL_DIR%/%INSTALL_DIR%/install" mkdir "%INSTALL_DIR%/%INSTALL
 if not exist "%INSTALL_DIR%/%INSTALL_DIR%/build" mkdir "%INSTALL_DIR%/%INSTALL_DIR%/build"
 
 echo [6/7] Configuration CMake
-cmake -S "%INSTALL_DIR%/%INSTALL_DIR%" -B "%INSTALL_DIR%/%INSTALL_DIR%/build" -G "Visual Studio 17 2022" -A x64 -DCMAKE_CONFIGURATION_TYPES="RelWithDebInfo" -DCMAKE_INSTALL_PREFIX="%cd%/%INSTALL_DIR%/%INSTALL_DIR%/install" -DGLEW_DIR="%cd%/glew-2.1.0/glew-2.1.0/install/lib/cmake/glew" -DPNG_INCLUDE_DIR="%cd%/libpng-1.6.47/libpng-1.6.47/myinstall/include" -DPNG_LIBRARY="%cd%/libpng-1.6.47/libpng-1.6.47/myinstall/lib/libpng16_static.lib" -DZLIB_LIBRARY="%cd%/zlib-1.3.1/zlib-1.3.1/install/lib/zlibstatic.lib" -Dglfw3_DIR="%cd%/glfw-master/glfw-master/install/lib/cmake/glfw3" -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake -S "%INSTALL_DIR%/%INSTALL_DIR%" -B "%INSTALL_DIR%/%INSTALL_DIR%/build" -G "Visual Studio 17 2022" -A x64 -DCMAKE_CONFIGURATION_TYPES="RelWithDebInfo" -DCMAKE_INSTALL_PREFIX="%cd%/%INSTALL_DIR%/%INSTALL_DIR%/install" -DGLEW_DIR="%cd%/glew-2.1.0/glew-2.1.0/install/lib/cmake/glew" -DPNG_INCLUDE_DIR="%cd%/libpng-1.6.47/libpng-1.6.47/myinstall/include" -DPNG_LIBRARY="%cd%/libpng-1.6.47/libpng-1.6.47/myinstall/lib/libpng16_static.lib" -DZLIB_LIBRARY="%cd%/zlib-1.3.1/zlib-1.3.1/install/lib/zlibstatic.lib" -Dglfw3_DIR="%cd%/glfw-master/glfw-master/install/lib/cmake/glfw3" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXXFLAGS="/D_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR"
 
 echo [7/7] Building
 cmake --build "%cd%/%INSTALL_DIR%/%INSTALL_DIR%/build" --config RelWithDebInfo --target install
