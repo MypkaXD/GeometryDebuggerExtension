251fd9ce9a975bb5b33b61f4dcc79a06304299b5
MypkaXD <nicklayschannel@yandex.ru>
2025-06-17 18:55:00 +0300

    test

--- .scripts/install_zlib.bat
+++ .scripts/install_zlib.bat
@@ -21,9 +21,9 @@ if not exist "%INSTALL_DIR%/%INSTALL_DIR%/install" mkdir "%INSTALL_DIR%/%INSTALL
 if not exist "%INSTALL_DIR%/%INSTALL_DIR%/build" mkdir "%INSTALL_DIR%/%INSTALL_DIR%/build"
 
 echo [5/6] Configuration CMake
-cmake -S "%INSTALL_DIR%/%INSTALL_DIR%" -B "%INSTALL_DIR%/%INSTALL_DIR%/build" -G "Visual Studio 17 2022" -A x64 -DCMAKE_CONFIGURATION_TYPES="Debug" -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%/%INSTALL_DIR%/install" -DINSTALL_BIN_DIR="%INSTALL_DIR%/%INSTALL_DIR%/install/bin" -DINSTALL_INC_DIR="%INSTALL_DIR%/%INSTALL_DIR%/install/include" -DINSTALL_LIB_DIR="%INSTALL_DIR%/%INSTALL_DIR%/install/lib" -DINSTALL_MAN_DIR="%INSTALL_DIR%/%INSTALL_DIR%/install/share/man" -DINSTALL_PKGCONFIG_DIR="%INSTALL_DIR%/%INSTALL_DIR%/install/share/pkgconfig" -DZLIB_BUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=Debug 
+cmake -S "%INSTALL_DIR%/%INSTALL_DIR%" -B "%INSTALL_DIR%/%INSTALL_DIR%/build" -G "Visual Studio 17 2022" -A x64 -DCMAKE_CONFIGURATION_TYPES="Release" -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%/%INSTALL_DIR%/install" -DINSTALL_BIN_DIR="%INSTALL_DIR%/%INSTALL_DIR%/install/bin" -DINSTALL_INC_DIR="%INSTALL_DIR%/%INSTALL_DIR%/install/include" -DINSTALL_LIB_DIR="%INSTALL_DIR%/%INSTALL_DIR%/install/lib" -DINSTALL_MAN_DIR="%INSTALL_DIR%/%INSTALL_DIR%/install/share/man" -DINSTALL_PKGCONFIG_DIR="%INSTALL_DIR%/%INSTALL_DIR%/install/share/pkgconfig" -DZLIB_BUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=Release 
 
 echo [6/6] Building
-cmake --build "%INSTALL_DIR%/%INSTALL_DIR%/build" --config Debug --target install
+cmake --build "%INSTALL_DIR%/%INSTALL_DIR%/build" --config Release --target install
 
 echo Ready! Library in %INSTALL_DIR%/%INSTALL_DIR%/install
